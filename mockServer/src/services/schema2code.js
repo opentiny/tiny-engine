7b149759b0c3d8ef2daf808cfc08cee2ec640c0a
ajaxzheng <894103554@qq.com>
2023-09-26 08:40:44 +0800

    fix: fix build error (#1)
    * fix: fix build error
    
    * fix: fix server dependence

--- mockServer/src/services/schema2code.js
+++ mockServer/src/services/schema2code.js
@@ -1,14 +1,14 @@
 /**
-* Copyright (c) 2023 - present TinyEngine Authors.
-* Copyright (c) 2023 - present Huawei Cloud Computing Technologies Co., Ltd.
-*
-* Use of this source code is governed by an MIT-style license.
-*
-* THE OPEN SOURCE SOFTWARE IN THIS PRODUCT IS DISTRIBUTED IN THE HOPE THAT IT WILL BE USEFUL,
-* BUT WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF MERCHANTABILITY OR FITNESS FOR
-* A PARTICULAR PURPOSE. SEE THE APPLICABLE LICENSES FOR MORE DETAILS.
-*
-*/
+ * Copyright (c) 2023 - present TinyEngine Authors.
+ * Copyright (c) 2023 - present Huawei Cloud Computing Technologies Co., Ltd.
+ *
+ * Use of this source code is governed by an MIT-style license.
+ *
+ * THE OPEN SOURCE SOFTWARE IN THIS PRODUCT IS DISTRIBUTED IN THE HOPE THAT IT WILL BE USEFUL,
+ * BUT WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF MERCHANTABILITY OR FITNESS FOR
+ * A PARTICULAR PURPOSE. SEE THE APPLICABLE LICENSES FOR MORE DETAILS.
+ *
+ */
 
 import { pageService } from '../routes/main-routes'
 import appInfo from './appinfo.json'
@@ -50,7 +50,7 @@ export default class Schema2CodeServcice {
       main: defaultMain
     })
 
-    const { generateCode } = require('@opentiny/lowcode-dsl-vue')
+    const { generateCode } = require('@opentiny/tiny-engine-dsl-vue')
     let code
     try {
       code = generateCode({
