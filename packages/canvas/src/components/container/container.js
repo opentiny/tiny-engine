407bc4eaf25c01d55bdee6adb8979b071580b4bb
gargameljyh <1954914724@qq.com>
2023-11-25 01:20:18 -0800

    fix: verify operations copy  id

--- packages/canvas/src/components/container/container.js
+++ packages/canvas/src/components/container/container.js
@@ -249,9 +249,6 @@ export const addComponent = (data, position) => {
 }
 
 export const copyNode = (id) => {
-  if (!id) {
-    return
-  }
   const { node, parent } = getNode(id, true)
 
   inserAfter({ parent, node, data: copyObject(node) })
