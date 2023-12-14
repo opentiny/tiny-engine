ec8faf03463fc305125e72ae216fa6b7a6970ac9
chilingling <michaelchiling1024@gmail.com>
2023-12-14 00:38:45 -0800

    fix(preview): import map add esm cdn

--- packages/design-core/src/preview/src/preview/importMap.js
+++ packages/design-core/src/preview/src/preview/importMap.js
@@ -18,11 +18,11 @@ const importMap = {}
 
 const tinyVue3Imports = {
   // 推荐之后统一使用@opentiny/vue去引入依赖，兼容后续录入的组件来源于tiny-vue
-  '@opentiny/vue': 'https://unpkg.com/@opentiny/vue@3/runtime/tiny-vue.mjs',
-  '@opentiny/vue-icon': 'https://unpkg.com/@opentiny/vue@3/runtime/tiny-vue-icon.mjs',
-  '@opentiny/vue-common': 'https://unpkg.com/@opentiny/vue@3/runtime/tiny-vue-common.mjs',
-  '@opentiny/vue-locale': 'https://unpkg.com/@opentiny/vue@3/runtime/tiny-vue-locale.mjs',
-  '@opentiny/vue-renderless/': 'https://unpkg.com/@opentiny/vue-renderless@3/'
+  '@opentiny/vue': 'https://registry.npmmirror.com/@opentiny/vue/3.11/files/runtime/tiny-vue.mjs',
+  '@opentiny/vue-icon': 'https://registry.npmmirror.com/@opentiny/vue/3.11/files/runtime/tiny-vue-icon.mjs',
+  '@opentiny/vue-common': 'https://registry.npmmirror.com/@opentiny/vue/3.11/files/runtime/tiny-vue-common.mjs',
+  '@opentiny/vue-locale': 'https://registry.npmmirror.com/@opentiny/vue/3.11/files/runtime/tiny-vue-locale.mjs',
+  '@opentiny/vue-renderless/': 'https://esm.sh/@opentiny/vue-renderless@3/?raw'
 }
 
 importMap.imports = {
@@ -31,7 +31,7 @@ importMap.imports = {
     'https://registry.npmmirror.com/vue/server-renderer/3.2.36/files/dist/server-renderer.esm-browser.js',
   'vue-i18n': 'https://registry.npmmirror.com/vue-i18n/9.2.0/files/dist/vue-i18n.esm-browser.js',
   'vue-router': 'https://registry.npmmirror.com/vue-router/4.0.16/files/dist/vue-router.esm-browser.js',
-  '@vue/devtools-api': 'https://unpkg.com/@vue/devtools-api@6.1.4/lib/esm/index.js',
+  '@vue/devtools-api': 'https://esm.sh/@vue/devtools-api@6.1.4/lib/esm/index.js?raw',
   '@vueuse/core': 'https://registry.npmmirror.com/@vueuse/core/9.6.0/files/index.mjs',
   '@vueuse/shared': 'https://registry.npmmirror.com/@vueuse/shared/9.6.0/files/index.mjs',
   axios: 'https://registry.npmmirror.com/axios/1.0.0-alpha.1/files/dist/esm/axios.js',
