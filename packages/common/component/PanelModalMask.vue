7df9abea75e7ce8ef5e4b098377c4e097a52fa55
wenmine <wwmmail@foxmail.com>
2024-01-23 15:39:36 +0800

    bugfix(panelMask): 修复属性面板遮罩层的宽度问题

--- packages/common/component/PanelModalMask.vue
+++ packages/common/component/PanelModalMask.vue
@@ -60,7 +60,7 @@ export default {
     top: 0;
     right: 0;
     bottom: 0;
-    left: calc(100% - var(--lowcode-setting-panel-min-width));
+    left: calc(100% - var(--base-right-panel-width));
     background: rgba(0, 0, 0, 0.15);
     z-index: 9999;
   }
