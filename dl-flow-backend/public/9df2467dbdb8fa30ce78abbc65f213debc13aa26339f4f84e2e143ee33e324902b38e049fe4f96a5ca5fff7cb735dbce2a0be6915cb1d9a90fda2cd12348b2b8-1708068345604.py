f5a58d2bc138e19b4c9aa49a0c2dea68ea2e8976
GaoNeng-wWw <gaonenggg@outlook.com>
2024-02-16 16:01:43 +0800

    feat(backend): remove generate res

--- dl-flow-backend/public/9df2467dbdb8fa30ce78abbc65f213debc13aa26339f4f84e2e143ee33e324902b38e049fe4f96a5ca5fff7cb735dbce2a0be6915cb1d9a90fda2cd12348b2b8-1708068345604.py
+++
@@ -1,7 +0,0 @@
-import paddle
-from paddle import *
-true = True
-false = False
-nodebd3ea921e76c4f09855fc65a50c02413 = paddle.nn.Conv1D(in_channels = 255,out_channels = 255,kernal_size = 3,stride = 1,padding_mode = 'zeros',weight_attr = ParamAttr(learning_rate = 1,regularizer = 'L1Decay',trainable = true,do_model_average = true,need_clip = true),bias_attr = ParamAttr(learning_rate = 1,regularizer = 'L1Decay',trainable = true,do_model_average = true,need_clip = true))
-node9415acf5bb5144cebce9aa6b54a58d59 = paddle.nn.AdaptiveAvgPool1D()
-node4efb497dd9f9473189ace9dcd5e73cad = paddle.nn.ReLU()
