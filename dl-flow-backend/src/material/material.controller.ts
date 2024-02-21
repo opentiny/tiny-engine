2319cf94effa1a1594dced9237a9d13cb633f969
GaoNeng-wWw <gaonenggg@outlook.com>
2024-02-21 21:04:56 +0800

    feat(frontend): get material

--- dl-flow-backend/src/material/material.controller.ts
+++ dl-flow-backend/src/material/material.controller.ts
@@ -1,9 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { MaterialService } from './material.service';
 
 @Controller('material')
 export class MaterialController {
   constructor(private readonly materialService: MaterialService) {}
+  @Get()
   getAll() {
     return this.materialService.findAll();
   }
