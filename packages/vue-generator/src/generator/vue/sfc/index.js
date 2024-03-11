69b7ccccd0c84224eac3a418c10c1e6578682260
chilingling <michaelchiling1024@gmail.com>
2024-03-11 06:27:16 -0700

    feat(vue-generator): add generate script frame code

---
+++ packages/vue-generator/src/generator/vue/sfc/index.js
@@ -0,0 +1,36 @@
+const handleComponentHook = () => {}
+
+const handleAttributeHook = () => {}
+
+const handleChildrenHook = () => {}
+
+const injectStatement = () => {}
+
+const injectMethod = () => {}
+
+const injectState = () => {}
+
+const injectImport = () => {}
+
+const setScriptConfig = () => {}
+
+const setCssConfig = () => {}
+
+const injectCss = () => {}
+
+const generateSFCFile = (schema, componentsMap, config) => {
+  // 前置动作
+  // 解析 import
+  // 解析 state
+  // 解析 method
+  //
+
+  // config
+  const scriptConfig = {}
+
+  const styleConfig = {}
+
+  const globalHooks = {}
+}
+
+export default generateSFCFile
--- packages/vue-generator/src/generator/vue/template/generateScript.js
+++
@@ -1,9 +0,0 @@
-export const generateSetupScript = () => {
-  // generate import statement
-  // props 声明
-  // emits 声明
-  // resource 工具类绑定
-  // reactive State 页面变量绑定声明
-  // js 方法声明绑定
-  // 生命周期绑定
-}
