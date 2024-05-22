e0b232714ea6ede5da039bd9308b9f46e01fb301
chilingling <michaelchiling1024@gmail.com>
2024-05-21 20:38:21 -0700

    fix(setting-panel): add meta for setting panel

---
+++ packages/settings/events/meta.js
@@ -0,0 +1,8 @@
+export default {
+  id: 'engine.setting.SettingEvents',
+  title: '高级',
+  type: 'setting',
+  align: 'left',
+  name: 'event',
+  icon: ''
+}
---
+++ packages/settings/styles/meta.js
@@ -0,0 +1,8 @@
+export default {
+  id: 'engine.setting.styles',
+  title: '样式',
+  name: 'style',
+  type: 'setting',
+  align: 'left',
+  icon: ''
+}
