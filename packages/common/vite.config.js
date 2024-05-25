514756e4abf8760a872ca0bedc04aa32b64dc2a7
rhlin <r.h.lin@163.com>
2024-05-25 01:19:46 -0700

    feat:(common,canvas,controller): 新增meta.js 和对应的打包修改 (#505)

--- packages/common/vite.config.js
+++ packages/common/vite.config.js
@@ -14,10 +14,11 @@ import { defineConfig } from 'vite'
 import path from 'path'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import generateComments from '@opentiny/vite-plugin-generate-comments'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), generateComments()],
   publicDir: false,
   resolve: {},
   build: {
