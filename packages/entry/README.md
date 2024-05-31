60dda4faf925582afe094a78af254e9415692b9a
chilingling <michaelchiling1024@gmail.com>
2024-05-30 20:50:23 -0700

    fix: refactor code by review

--- packages/entry/README.md
+++ packages/entry/README.md
@@ -1,6 +1,5 @@
 # entry(暂定)
 
-
 ## configurators
 
 往设计器添加 metaComponent，满足右侧属性面板属性设置器自定义组件的需求
--- packages/entry/src/configurators.js
+++ packages/entry/src/configurators.js
@@ -1,6 +1,7 @@
 const configuratorsMap = new Map()
 
 export const addConfigurator = (components) => {
+  // TODO: 数据结构校验&错误抛出
   if (Array.isArray(components)) {
     for (const { name, component } of components) {
       configuratorsMap.set(name, component)
