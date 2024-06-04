182e5f490b224eb7c187deed7a6b74e163b4d78c
chilingling <michaelchiling1024@gmail.com>
2024-06-04 04:49:03 -0700

    fix: fix some build issue
    1. fix cycle dependency between utils and entry
    2. change vite-plusin-generate-comments to
       tiny-engine-vite-plugin-meta-comments
    3. delete duplicate dependency inside design-core

--- packages/vue-generator/vite.config.mjs
+++ packages/vue-generator/vite.config.mjs
@@ -13,7 +13,7 @@
 import { defineConfig } from 'vite'
 import path from 'path'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
-import generateComment from '@opentiny/vite-plugin-generate-comments'
+import generateComment from '@opentiny/tiny-engine-vite-plugin-meta-comments'
 
 // https://vitejs.dev/config/
 export default defineConfig({
