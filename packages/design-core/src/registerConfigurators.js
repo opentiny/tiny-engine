b884cb86bf8e0046f05a249a28ce1843602e03c2
chilingling <michaelchiling1024@gmail.com>
2024-06-13 19:36:26 -0700

    feat: register all configurator by default

--- packages/design-core/src/registerConfigurators.js
+++ packages/design-core/src/registerConfigurators.js
@@ -1,3 +1,4 @@
+import * as allConfigurator from '@opentiny/tiny-engine-configurator'
 // TODO: 抽空 common package 中的 metaComponent
 import { MetaComponents } from '@opentiny/tiny-engine-common'
 import { addConfigurator } from '@opentiny/tiny-engine-entry'
@@ -7,5 +8,6 @@ import { addConfigurator } from '@opentiny/tiny-engine-entry'
  */
 export const registerConfigurators = (configurators) => {
   addConfigurator(Object.entries(MetaComponents).map(([name, component]) => ({ name, component })))
+  addConfigurator(Object.entries(allConfigurator).map(([name, component]) => ({ name, component })))
   addConfigurator(Object.entries(configurators).map(([name, component]) => ({ name, component })))
 }
