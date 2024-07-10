071e80c017bbb09fc2d78db246910a36bf63c7c3
chilingling <26962197+chilingling@users.noreply.github.com>
2024-07-10 01:13:54 -0700

    fix: dev without alias error (#647)
    * fix: dev without alias error
    
    * fix: usesource alias default to true

--- packages/build/vite-config/src/vite-plugins/devAliasPlugin.js
+++ packages/build/vite-config/src/vite-plugins/devAliasPlugin.js
@@ -60,13 +60,25 @@ const devAlias = {
   '@opentiny/tiny-engine-configurator': path.resolve(process.cwd(), '../packages/configurator/src/index.js')
 }
 
+const getThemePath = (theme, useSourceAlias) => {
+  if (!['light', 'dark'].includes(theme)) {
+    return ''
+  }
+
+  if (useSourceAlias) {
+    return path.resolve(process.cwd(), `../packages/theme/${theme}/index.less`)
+  }
+
+  return path.resolve(process.cwd(), `./node_modules/@opentiny/tiny-engine-theme-${theme}/dist/style.css`)
+}
+
 /**
  * 源码调试插件
  * 开启后，会指定  alias 到官方源码 package
  * @param {*} env
  * @returns
  */
-export const devAliasPlugin = (env) => {
+export const devAliasPlugin = (env, useSourceAlias) => {
   return {
     name: 'vite-plugin-dev-alias',
     config(config, { command }) {
@@ -75,10 +87,8 @@ export const devAliasPlugin = (env) => {
         return {
           resolve: {
             alias: {
-              ...devAlias,
-              '@opentiny/tiny-engine-theme': ['light', 'dark'].includes(env.VITE_THEME)
-                ? path.resolve(process.cwd(), `../packages/theme/${env.VITE_THEME}/index.less`)
-                : ''
+              ...(useSourceAlias ? devAlias : {}),
+              '@opentiny/tiny-engine-theme': getThemePath(env.VITE_THEME, useSourceAlias)
             }
           }
         }
@@ -88,12 +98,8 @@ export const devAliasPlugin = (env) => {
         return {
           resolve: {
             alias: {
-              '@opentiny/tiny-engine-theme': ['light', 'dark'].includes(env.VITE_THEME)
-                ? path.resolve(
-                    process.cwd(),
-                    `./node_modules/@opentiny/tiny-engine-theme-${env.VITE_THEME}/dist/style.css`
-                  )
-                : ''
+              // 构建不使用 alias
+              '@opentiny/tiny-engine-theme': getThemePath(env.VITE_THEME, false)
             }
           }
         }
