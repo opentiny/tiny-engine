492c4d3c02db68dbff81fc2f7e3d38dc13a9cd4e
hexqi <wu_12556@126.com>
2024-07-11 13:23:48 +0800

    fix: build

--- packages/common/component/MetaCodeEditor.vue
+++ packages/common/component/MetaCodeEditor.vue
@@ -66,12 +66,12 @@
 </template>
 
 <script>
-import VueMonaco from './VueMonaco'
-import { formatString } from '@opentiny/tiny-engine-common/js/ast'
-import i18n from '@opentiny/tiny-engine-common/js/i18n'
+import { computed, nextTick, reactive, ref, watchEffect } from 'vue'
 import { Button, DialogBox } from '@opentiny/vue'
 import { iconChevronDown, iconChevronUp } from '@opentiny/vue-icon'
-import { computed, nextTick, reactive, ref, watchEffect } from 'vue'
+import { formatString } from '@opentiny/tiny-engine-common/js/ast'
+import i18n from '@opentiny/tiny-engine-common/js/i18n'
+import VueMonaco from './VueMonaco.vue'
 
 export default {
   components: {
