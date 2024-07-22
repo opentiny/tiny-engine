ba1d0ee6ebec0583ec5868f6531d0733b56936ee
STATICHIT <2394412110@qq.com>
2024-07-22 17:38:13 +0800

    refactor(settings,events): 事件插件固定面板以及拖拽功能实现

--- packages/settings/events/src/Main.vue
+++ packages/settings/events/src/Main.vue
@@ -1,19 +1,54 @@
 <template>
-  <tiny-collapse v-model="activeNames">
-    <tiny-collapse-item title="事件绑定" name="bindEvent">
-      <bind-events></bind-events>
-    </tiny-collapse-item>
-    <tiny-collapse-item title="高级配置" name="advancedConfig">
-      <advance-config></advance-config>
-    </tiny-collapse-item>
-  </tiny-collapse>
+  <plugin-panel title="高级" :fixed-panels="fixedPanels" :fixed-name="SETTING_NAME.Event" :defaultWidth="320"  @close="$emit('close')">
+    <template #content>
+      <tiny-collapse v-model="activeNames">
+        <tiny-collapse-item title="事件绑定" name="bindEvent">
+          <bind-events></bind-events>
+        </tiny-collapse-item>
+        <tiny-collapse-item title="高级配置" name="advancedConfig">
+          <advance-config></advance-config>
+        </tiny-collapse-item>
+      </tiny-collapse>
+    </template>
+  </plugin-panel>
 </template>
 
-<script setup>
-import { ref } from 'vue'
+<!-- event插件界面 -->
+<script>
+import { ref, reactive, provide } from 'vue'
 import { Collapse as TinyCollapse, CollapseItem as TinyCollapseItem } from '@opentiny/vue'
 import BindEvents from './components/BindEvents.vue'
 import AdvanceConfig from './components/AdvanceConfig.vue'
+import { PluginPanel, SvgButton } from '@opentiny/tiny-engine-common'
+import { useLayout } from '@opentiny/tiny-engine-controller'
+export default {
+  components: {
+    PluginPanel,
+    SvgButton,
+    BindEvents,
+    AdvanceConfig,
+    TinyCollapse,
+    TinyCollapseItem
+  },
+  props: {
+    fixedPanels: {
+      type: Array
+    }
+  },
+  emits: ['close'],
+  setup(props, { emit }) {
+    const { SETTING_NAME } = useLayout()
+    const activeNames = ref(['bindEvent', 'advancedConfig'])
 
-const activeNames = ref(['bindEvent', 'advancedConfig'])
+    const panelState = reactive({
+      emitEvent: emit
+    })
+    provide('panelState', panelState)
+
+    return {
+      SETTING_NAME,
+      activeNames
+    }
+  }
+}
 </script>
