71ef42a0ca521d38f3d2bb1d285f824586c1b9f3
huyaohua <1525656754@qq.com>
2024-08-26 19:14:56 +0800

    feat: elementplus物料更新优化

--- packages/design-core/public/mock/genMeterials.js
+++ packages/design-core/public/mock/genMeterials.js
@@ -5,17 +5,139 @@ const axios = require('axios')
 const cheerio = require('cheerio')
 const fs = require('fs')
 
+function createConfigure(params) {
+  return {
+    loop: true,
+    condition: true,
+    styles: true,
+    isContainer: false,
+    isModal: false,
+    isPopper: false,
+    nestingRule: {
+      childWhitelist: '',
+      parentWhitelist: '',
+      descendantBlacklist: '',
+      ancestorWhitelist: ''
+    },
+    isNullNode: false,
+    isLayout: false,
+    rootSelector: '',
+    shortcuts: {
+      properties: params || []
+    },
+    contextMenu: {
+      actions: ['copy', 'remove', 'insert', 'updateAttr', 'bindEevent', 'createBlock'],
+      disable: []
+    },
+    invalidity: [''],
+    clickCapture: true,
+    framework: 'Vue'
+  }
+}
+
+function createSchema(params) {
+  // 这个函数的作用是：将参数转为
+  const result = {
+    properties: [],
+    events: {},
+    slots: {}
+  }
+  const widgetTypeMap = {
+    string: 'MetaInput',
+    enum: 'MetaSelect',
+    boolean: 'MetaSwitch',
+    function: 'MetaCodeEditor',
+    object: 'MetaCodeEditor'
+  }
+  const typeTypeMap = {
+    string: 'MetaInput',
+    enum: 'MetaSelect',
+    boolean: 'MetaSwitch',
+    function: 'MetaCodeEditor',
+    object: 'MetaCodeEditor'
+  }
+
+  // 处理属性
+  params.tables.forEach((table) => {
+    if (table.tableName.includes('属性') || table.tableName.includes('Attributes')) {
+      const propertyGroup = {
+        name: '0',
+        label: { zh_CN: table.tableName.trim() },
+        description: { zh_CN: table.tableName.trim() },
+        content: []
+      }
+
+      table.bodys.forEach((row) => {
+        const [property, description, type, defaultValue] = row
+        propertyGroup.content.push({
+          property: property.split(' ')[0],
+          label: {
+            text: { zh_CN: property.split(' ')[0] }
+          },
+          description: {
+            zh_CN: description
+          },
+          type: type === 'enum' ? 'string' : type,
+          required: true, // 根据你的需求，这个可以设置为 true 或 false
+          readOnly: false,
+          disabled: false,
+          cols: 12,
+          labelPosition: 'top',
+          widget: {
+            component: widgetTypeMap[type] || '__ERROR__',
+            props: {
+              slots: ['default']
+            }
+          }
+        })
+      })
+
+      result.properties.push(propertyGroup)
+    }
+
+    // 处理插槽
+    if (table.tableName.includes('插槽') || table.tableName.includes('Slots')) {
+      table.bodys.forEach(([slot, description, subTag]) => {
+        result.slots[slot] = {
+          label: { zh_CN: slot },
+          description: { zh_CN: description }
+        }
+        if (subTag) {
+          result.slots[slot].subTag = subTag
+        }
+      })
+    }
+
+    // 处理方法（如果需要的话）
+    if (table.tableName.includes('方法') || table.tableName.includes('Events')) {
+      table.bodys.forEach((row) => {
+        const [property, description, type] = row
+        // 在这里添加方法的处理逻辑
+        // 示例: result.methods[property] = { description, type };
+      })
+    }
+  })
+
+  return result
+}
+
 async function loadMenuData() {
   const url = 'https://element-plus.org/zh-CN/component/overview.html'
   const response = await axios.get(url)
   const html = response.data
   const $ = cheerio.load(html)
-  const sidebarItems = $('.sidebar-group a')
+  const sidebarItems = $('.sidebar-group')
   const data = []
+
   sidebarItems.each((index, element) => {
-    const link = `${baseUrl}${$(element).attr('href')}.html`
-    const text = $(element).text().trim()
-    data.push({ text, link })
+    const tagP = $(element).find('.sidebar-group__title')
+    const tagAList = $(element).find('a')
+    const tagPText = tagP.text()
+    tagAList.each((i, item) => {
+      const link = `${baseUrl}${$(item).attr('href')}.html`
+      const text = $(item).text().trim()
+      data.push({ text, link, group: tagPText })
+    })
   })
   return data.filter((itemStr) => itemStr.text !== 'Overview 组件总览')
 }
@@ -36,18 +158,23 @@ async function loadBaseDataByMenus(menus) {
     const html = response.data
     const $ = cheerio.load(html)
     const sidebarItems = $('.vp-table')
+    const desc = $('h1 + p').text()
     const tables = []
     let objName = {
       name: menu.text,
+      desc,
+      group: menu.group,
       tables
+      // children: []
     }
     sidebarItems.each((index, table) => {
       const thItems = $(table).find('tr th')
       const thtdItems = $(table).find('tr td')
+      const prevNodeTitle = $(table).prev().text()
       const headers = []
       let wrapBodys = []
       const bodys = []
-      const tableObj = { headers, bodys: [] }
+      const tableObj = { headers, bodys: [], tableName: prevNodeTitle }
       thItems.each((i, th) => {
         headers.push($(th).text().trim())
       })
@@ -58,6 +185,7 @@ async function loadBaseDataByMenus(menus) {
       tableObj.bodys = wrapBodys
       tables.push(tableObj)
     })
+    // objName.children = tables // Assuming tables correspond to children in this context
     result.push(objName)
   }
   return result
@@ -83,14 +211,18 @@ async function generateComponent(params) {
 
     const cleanedName = param.name.replace(/^\w+\s+/, '')
 
+    const getName = (str) => str.split(' ')[0]
+    const name = getName(param.name)
+
+    // Button
     const component = {
       id: components.length + 1,
       version: VERSION,
       name: { zh_CN: cleanedName },
-      component: param.name,
-      icon: '',
-      description: '',
-      doc_url: param.tables.length > 0 ? param.tables[0].link : '',
+      component: `El${name}`,
+      icon: String(name).toLowerCase(),
+      description: param.desc,
+      doc_url: '',
       screenshot: '',
       tags: '',
       keywords: '',
@@ -98,15 +230,15 @@ async function generateComponent(params) {
       npm: {
         package: 'element-plus',
         version: VERSION,
-        exportName: param.name,
-        main: 'lib/index.js',
-        destructuring: true,
-        subName: ''
+        script: 'https://npm.onmicrosoft.cn/element-plus@2.4.2/dist/index.full.mjs',
+        css: 'https://npm.onmicrosoft.cn/element-plus@2.4.2/dist/index.css',
+        dependencies: null,
+        exportName: `El${name}`
       },
-      group: '',
+      group: param.group,
       category: 'element-plus',
-      configure: {},
-      schema: {}
+      configure: createConfigure(),
+      schema: createSchema(param)
     }
     components.push(component)
   }
@@ -115,25 +247,34 @@ async function generateComponent(params) {
 
 async function generateSnippets(params) {
   const snippets = []
-
+  const snippetChildrens = []
   for (let i = 0; i < params.length; i++) {
     const param = params[i]
-    const snippet = {
-      id: snippets.length + 1,
-      name: { zh_CN: `使用${param.name}` },
-      description: `如何在Vue中使用${param.name}`,
-      code: `<template>\n  <${param.name}></${param.name}>\n</template>`,
-      language: 'Vue'
+
+    const snippetChildren = {
+      name: { zh_CN: param.name['zh_CN'] },
+      icon: param.icon, // Default icon, you can customize it
+      screenshot: '', // Screenshot URL or path, if available
+      snippetName: param.component,
+      schema: {} // Schema structure, populate as needed
     }
-    snippets.push(snippet)
+    snippetChildrens.push(snippetChildren)
   }
+
+  const snippet = {
+    group: 'element-plus',
+    children: snippetChildrens
+  }
+
+  snippets.push(snippet)
+
   return snippets
 }
 
 async function generateMaterial(params) {
   const components = await generateComponent(params)
   const blocks = []
-  const snippets = await generateSnippets(params)
+  const snippets = await generateSnippets(components)
   const result = {
     framework: 'Vue',
     materials: {
@@ -148,7 +289,8 @@ async function generateMaterial(params) {
 async function main() {
   try {
     const menus = await loadMenuData()
-    const x = await loadBaseDataByMenus(menus)
+    const onlyFirst = menus.slice(0, 1)
+    const x = await loadBaseDataByMenus(onlyFirst)
     const m = await generateMaterial(x)
     generateJSONFile({ data: m }, 'bundle.json', './')
   } catch (error) {
