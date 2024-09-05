019ea140783915761f8a9a61a1ec625ca9e26d32
youbo <42180410+spongebody@users.noreply.github.com>
2024-09-05 14:46:59 +0800

    fix(plugins/template): 修复添加模板类别无效问题 (#790)
    * fix(plugins/template): 修复添加模板类别无效问题
    
    * refactor(plugins/tempalte): 代码优化

--- packages/plugins/template/src/TemplateSetting.vue
+++ packages/plugins/template/src/TemplateSetting.vue
@@ -99,7 +99,7 @@ export default {
           fileName: templateSettingState.currentTemplateData.name
         },
         app: appInfoState.selectedId,
-        isTemplate: true
+        isTemplate: templateSettingState.currentTemplateData.type === 'template'
       }
 
       if (createParams.id) {
