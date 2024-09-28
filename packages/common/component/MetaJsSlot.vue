4ea63abba72b40c76265cde7e8a8c48b92b5f8ac
tianxin <146069396+ianxinnew@users.noreply.github.com>
2024-09-28 18:34:24 +0800

    fix:table slot edit params should not get alert (#802)
    * fix:table notch saving error message
    
    * fix: change slotparams cause warning
    
    * fix: change by review
    
    * fix: typo parma -> param
    
    ---------
    
    Co-authored-by: chilingling <michaelchiling1024@gmail.com>

--- packages/common/component/MetaJsSlot.vue
+++ packages/common/component/MetaJsSlot.vue
@@ -17,13 +17,13 @@
           </div>
           <tiny-form-item
             :prop="paramsPropPath(index)"
-            :rules="[{ validator: parmasStringValidator, trigger: 'blur' }]"
+            :rules="[{ validator: paramsStringValidator, trigger: 'blur' }]"
             class="slot-name-form-item"
           >
             <tiny-input
               v-model="slot.params"
               class="use-slot-params"
-              @change="validParmas(slot, paramsPropPath(index))"
+              @change="validParams(paramsPropPath(index), slot)"
             ></tiny-input>
           </tiny-form-item>
         </div>
@@ -84,15 +84,59 @@ export default {
 
     const paramsPropPath = (index) => `${index}.params`
 
-    const parmasStringValidator = (rule, value, callback) => {
-      if (value && value.split(',').some((parma) => !verifyJsVarName(parma))) {
+    const paramsStringValidator = (rule, value, callback) => {
+      if (value && value.split(',').some((param) => !verifyJsVarName(param))) {
         callback(new Error('仅支持JavaScript中有效的变量名'))
       } else {
         callback()
       }
     }
 
+    const updateSlotParams = (slotData) => {
+      emit('update:modelValue', slotData)
+
+      // 更新当前选中组件的根属性，不更新在jsslot中的数据非响应式
+      const [propsName] = path.split('.')
+      const schema = useProperties().getSchema()
+      schema.props[propsName] = JSON.parse(JSON.stringify(schema.props[propsName]))
+    }
+
+    const setSlotParams = ({ name, params = '' }) => {
+      if (!props.modelValue?.[name]) {
+        return
+      }
+
+      const slotData = { ...(props.modelValue || {}) }
+
+      if (params.length) {
+        slotData[name].params = params.split(',')
+      } else {
+        delete slotData[name].params
+      }
+
+      updateSlotParams(slotData)
+    }
+
     const toggleSlot = (idx, { bind, name, params = '' }) => {
+      // 原本绑定的，解除绑定
+      if (bind) {
+        useModal().confirm({
+          title: '提示',
+          message: '关闭后插槽内的内容将被清空，是否继续？',
+          status: 'info',
+          exec: () => {
+            slotList.value[idx].bind = false
+            const { [name]: _deleted, ...rest } = { ...(props.modelValue || {}) }
+            updateSlotParams(rest)
+          }
+        })
+
+        return
+      }
+
+      // 未绑定的，新增绑定
+      slotList.value[idx].bind = true
+
       const slotInfo = {
         [name]: {
           type: 'JSSlot',
@@ -104,47 +148,17 @@ export default {
         }
       }
 
-      const slotData = { ...slotInfo, ...(props.modelValue || {}) }
-
       if (params.length) {
-        slotData[name].params = params.split(',')
-      } else {
-        delete slotData[name].params
+        slotInfo[name].params = params.split(',')
       }
-      if (bind) {
-        useModal().confirm({
-          title: '提示',
-          message: '关闭后插槽内的内容将被清空，是否继续？',
-          status: 'info',
-          exec: () => {
-            slotList.value[idx].bind = false
-            delete slotData[name]
-            emit('update:modelValue', slotData)
-            const [propsName] = path.split('.')
-            const schema = useProperties().getSchema()
-            schema.props[propsName] = JSON.parse(JSON.stringify(schema.props[propsName]))
-          },
-          cancel: () => {}
-        })
-      } else {
-        slotList.value[idx].bind = true
-      }
-      emit('update:modelValue', slotData)
 
-      // 更新当前选中组件的根属性，不根新在jsslot中的数据非响应式
-      const [propsName] = path.split('.')
-      const schema = useProperties().getSchema()
-      schema.props[propsName] = JSON.parse(JSON.stringify(schema.props[propsName]))
+      updateSlotParams({ ...(props.modelValue || {}), ...slotInfo })
     }
 
-    const setParams = (slot) => {
-      slot.bind && toggleSlot(true, slot)
-    }
-
-    const validParmas = (slot, parmasPath) => {
-      slotRef.value.validateField([parmasPath], (tips) => {
-        if (!tips) {
-          setParams(slot)
+    const validParams = (paramsPath, slot) => {
+      slotRef.value.validateField([paramsPath], (error) => {
+        if (!error) {
+          slot.bind && setSlotParams(slot)
         }
       })
     }
@@ -159,9 +173,8 @@ export default {
       slotList,
       paramsPropPath,
       slotRef,
-      parmasStringValidator,
-      validParmas,
-      setParams,
+      paramsStringValidator,
+      validParams,
       state,
       componentsMap
     }
