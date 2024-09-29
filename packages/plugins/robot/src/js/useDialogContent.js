a2e07d4ccecd47829fe0fb87931dd045cf3c2e29
wangjunyue <984209872@qq.com>
2024-09-30 07:24:40 +0800

    feat: modify the document

--- packages/plugins/robot/src/js/useDialogContent.js
+++ packages/plugins/robot/src/js/useDialogContent.js
@@ -24,46 +24,21 @@ export default function useMarkdown() {
         highlighted = md.utils.escapeHtml(str)
       }
       const copyButtonText = '复制'
-      const generateButtonText = '生成schema'
-      return `<div class="code-block"><pre class="hljs code-container"><code>${highlighted}</code></pre><button class="generate-btn code-block-btn">${generateButtonText}</button><button class="copy-btn code-block-btn">${copyButtonText}</button></div>`
+      return `<div class="code-block"><pre class="hljs code-container"><code>${highlighted}</code></pre><button class="copy-btn code-block-btn">${copyButtonText}</button></div>`
     }
   })
 
   function initClipboard() {
-    const generate = new ClipboardJS('.generate-btn', {
-      text: function (trigger) {
-        if (trigger.classList.contains('generate-schema-btn')) {
-          // 处理生成 schema 的逻辑
-          Notify({
-            type: 'success',
-            message: '已生成',
-            position: 'top-right',
-            duration: 3000
-          })
-          return '' // 不需要复制的内容
-        }
-        return '生成schema了'
-      }
-    })
-
-    generate.on('success', function (e) {
-      const originalText = '生成schema'
-      e.trigger.textContent = '已生成'
-      setTimeout(() => {
-        e.trigger.textContent = originalText
-      }, 3000)
-      e.clearSelection()
-    })
-
     const clipboard = new ClipboardJS('.copy-btn', {
       text: function (trigger) {
-        return trigger.closest('.code-block').querySelector('.hljs').textContent
+        return trigger.previousElementSibling.textContent
       }
     })
 
     clipboard.on('success', function (e) {
       const originalText = '复制'
-      e.trigger.textContent = '已复制'
+      const copiedText = '已复制'
+      e.trigger.textContent = copiedText
       setTimeout(() => {
         e.trigger.textContent = originalText
       }, 3000)
