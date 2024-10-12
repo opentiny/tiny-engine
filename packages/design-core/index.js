d244ae5147cbf149b0d8a098ea536e498aa6b5c2
yy-wow <yaoyun_bigger@outlook.com>
2024-10-12 01:45:40 -0700

    fix: export createRender

--- packages/design-core/index.js
+++ packages/design-core/index.js
@@ -32,6 +32,7 @@ export { default as Events } from '@opentiny/tiny-engine-setting-events'
 export { default as Styles } from '@opentiny/tiny-engine-setting-styles'
 export { default as Layout, LayoutService } from '@opentiny/tiny-engine-layout'
 export { default as Canvas } from '@opentiny/tiny-engine-canvas'
+export { createRender } from '@opentiny/tiny-engine-canvas/render'
 export { initPreview } from './src/preview/src/main'
 export { PluginPanel, ToolbarBase } from '@opentiny/tiny-engine-common'
 
