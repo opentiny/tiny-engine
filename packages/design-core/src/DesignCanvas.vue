bad2cd61445bcab5eda1db70e1149a56e6876dd6
STATICHIT <2394412110@qq.com>
2024-10-20 21:12:28 +0800

    optimize: 绑定插件根据固定状态判断点击画布时是否关闭代码逻辑

--- packages/design-core/src/DesignCanvas.vue
+++ packages/design-core/src/DesignCanvas.vue
@@ -135,11 +135,15 @@ export default {
     )
 
     const nodeSelected = (node, parent, type) => {
-      const { toolbars } = useLayout().layoutState
-      if (type !== 'clickTree') {
-        useLayout().closePlugin()
+      const { toolbars, plugins, settings } = useLayout().layoutState
+      const leftPanels = localStorage.getItem('leftPanels')
+      const rightPanels = localStorage.getItem('rightPanels')
+      if (type !== 'clickTree' && plugins.render && !leftPanels.includes(plugins.render)) {
+        useLayout().closePlugin(true)
       }
-
+      if (settings.render && !rightPanels.includes(settings.render)) {
+        useLayout().closeSetting(true)
+      }
       const { getSchema, getNodePath } = useCanvas().canvasApi.value
 
       const schema = getSchema()
