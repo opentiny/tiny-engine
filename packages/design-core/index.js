399b9ade7492c5dd62b0019e732bae0c3657ad6e
betterdancing <liulinclear@163.com>
2024-10-22 10:10:50 +0800

    Feat(page): Add exporting components and APIs for page component to support page template extension (alpha version) (#797)
    * feat(page): 新增页面管理导出components,扩展AstroPro页面模板功能
    
    * Feat(page): 删除pageMain.vue，如需扩展直接使用entry
    
    * feat(plugin-page): pageSetting暂时修改以适配扩展改造
    
    * feat(plugin-page): pageSetting暂时修改以适配扩展改造
    
    * feat(plugin-page): 修改api数组为对象格式

--- packages/design-core/index.js
+++ packages/design-core/index.js
@@ -33,7 +33,7 @@ export { default as Styles } from '@opentiny/tiny-engine-setting-styles'
 export { default as Layout, LayoutService } from '@opentiny/tiny-engine-layout'
 export { default as Canvas } from '@opentiny/tiny-engine-canvas'
 export { initPreview } from './src/preview/src/main'
-export { PluginPanel, ToolbarBase } from '@opentiny/tiny-engine-common'
+export { PluginPanel, PluginSetting, ToolbarBase } from '@opentiny/tiny-engine-common'
 
 export { default as defaultRegistry } from './registry'
 
