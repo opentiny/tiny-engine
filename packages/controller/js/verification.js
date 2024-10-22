00cdbbab97345350131acc2a1bca2d22cc8cc3dd
chilingling <26962197+chilingling@users.noreply.github.com>
2024-10-21 20:49:39 -0700

    fix: blockName regexp incorrect (#852)
    * fix: blockName regexp incorrect
    
    * fix: change regexp by review
    
    * fix: change by review
    
    * fix: change by review comment

--- packages/controller/js/verification.js
+++ packages/controller/js/verification.js
@@ -1,20 +1,20 @@
 /**
-* Copyright (c) 2023 - present TinyEngine Authors.
-* Copyright (c) 2023 - present Huawei Cloud Computing Technologies Co., Ltd.
-*
-* Use of this source code is governed by an MIT-style license.
-*
-* THE OPEN SOURCE SOFTWARE IN THIS PRODUCT IS DISTRIBUTED IN THE HOPE THAT IT WILL BE USEFUL,
-* BUT WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF MERCHANTABILITY OR FITNESS FOR
-* A PARTICULAR PURPOSE. SEE THE APPLICABLE LICENSES FOR MORE DETAILS.
-*
-*/
+ * Copyright (c) 2023 - present TinyEngine Authors.
+ * Copyright (c) 2023 - present Huawei Cloud Computing Technologies Co., Ltd.
+ *
+ * Use of this source code is governed by an MIT-style license.
+ *
+ * THE OPEN SOURCE SOFTWARE IN THIS PRODUCT IS DISTRIBUTED IN THE HOPE THAT IT WILL BE USEFUL,
+ * BUT WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF MERCHANTABILITY OR FITNESS FOR
+ * A PARTICULAR PURPOSE. SEE THE APPLICABLE LICENSES FOR MORE DETAILS.
+ *
+ */
 
 export const REGEXP_EVENT_NAME = /^[a-z]+([A-Z][a-z]*)*$/
 
 export const verifyEventName = (name) => REGEXP_EVENT_NAME.test(name)
 
-export const REGEXP_BLOCK_NAME = /^([A-Z][A-Za-z0-9]{2,})*?([A-Z][A-Za-z0-9]{2,})*?$/
+export const REGEXP_BLOCK_NAME = /^([A-Z][a-z0-9]*){2,}$/
 
 export const verifyBlockName = (string) => REGEXP_BLOCK_NAME.test(string)
 
