8055d3c4a42350d1db7200aceec2f3ab85bd8322
yy-wow <yaoyun_bigger@outlook.com>
2024-10-24 02:18:37 -0700

    refactor: split renderer package

---
+++ packages/renderer/src/builtin/CanvasSlot.vue
@@ -0,0 +1,22 @@
+<template>
+  <slot>
+    <canvas-placeholder v-bind="$attrs"></canvas-placeholder>
+  </slot>
+</template>
+
+<script>
+import CanvasPlaceholder from './CanvasPlaceholder.vue'
+export default {
+  components: {
+    CanvasPlaceholder
+  },
+  props: {
+    name: {
+      type: String
+    },
+    params: {
+      type: String
+    }
+  }
+}
+</script>
---
+++ packages/renderer/src/constants.js
@@ -0,0 +1,3 @@
+export const NODE_UID = 'data-uid'
+export const NODE_TAG = 'data-tag'
+export const NODE_LOOP = 'loop-id'
