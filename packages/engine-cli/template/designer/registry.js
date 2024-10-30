c193866c9a64e8828ef049fa871f0ec522088a3f
yy-wow <yaoyun_bigger@outlook.com>
2024-10-29 20:11:15 -0700

    feat: update cli template

--- packages/engine-cli/template/designer/registry.js
+++ packages/engine-cli/template/designer/registry.js
@@ -45,11 +45,12 @@ import {
   GlobalService
 } from '@opentiny/tiny-engine'
 import engineConfig from './engine.config'
+import { HttpService } from './src/composable'
 
 export default {
   root: {
     id: 'engine.root',
-    metas: [GenerateCodeService, GlobalService]
+    metas: [HttpService, GenerateCodeService, GlobalService]
   },
   config: engineConfig,
   layout: {
