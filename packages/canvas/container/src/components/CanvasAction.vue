45d1f9967fd50e8609d770f3344a1a364e989000
yy-wow <yaoyun_bigger@outlook.com>
2024-12-27 01:02:35 -0800

    fix: 快捷操作属性配置国际化和图标配置异常关闭

--- packages/canvas/container/src/components/CanvasAction.vue
+++ packages/canvas/container/src/components/CanvasAction.vue
@@ -261,11 +261,13 @@ export default {
 
       let name = JSON.stringify(parent.className)
 
-      if (name && name.indexOf('short-cut-set') === -1 && name.indexOf('tiny-dialog-box') === -1) {
-        flag = findParentHasClass(parent)
-      } else {
-        flag = true
+      const preventClassNameList = ['short-cut-set', 'tiny-dialog-box', 'icon-popover', 'i18n-input-popover']
+
+      if (preventClassNameList.some((item) => name?.includes(item))) {
+        return true
       }
+
+      flag = findParentHasClass(parent)
 
       return flag
     }
