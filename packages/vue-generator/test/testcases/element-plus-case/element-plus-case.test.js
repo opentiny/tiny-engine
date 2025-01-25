a7a2e14cd05c1dbe58f4295a08e99b29d7ad0798
chilingling <michaelchiling1024@gmail.com>
2025-01-25 01:54:21 -0800

    fix: gencode element-plus test case

--- packages/vue-generator/test/testcases/element-plus-case/element-plus-case.test.js
+++ packages/vue-generator/test/testcases/element-plus-case/element-plus-case.test.js
@@ -1,10 +1,21 @@
-import { expect, test, describe } from 'vitest'
+import { expect, test, describe, vi } from 'vitest'
 import path from 'path'
 import fs from 'fs'
 import dirCompare from 'dir-compare'
 import { generateApp } from '@/generator/generateApp'
 import { appSchemaDemo01 } from './mockData'
 import { logDiffResult } from '../../utils/logDiffResult'
+
+// 需要模拟 import 出来的 favicon.ico 文件， base64 格式，因为在 test 运行环境中直接的 import 出来的不是 bast64 字符串，而是路径
+vi.mock('../../../src/templates/vue-template/templateFiles/public/favicon.ico', () => {
+  const faviconRelativePath = '../../../src/templates/vue-template/templateFiles/public/favicon.ico'
+  const fileBuffer = fs.readFileSync(path.join(__dirname, faviconRelativePath))
+  const str = fileBuffer.toString('base64')
+
+  return {
+    default: `data:image/x-icon;base64,${str}`
+  }
+})
 
 describe('generate element-plus material project correctly', () => {
   test('should generate element-plus css import statement 预期生成 element-plus 样式依赖引入', async () => {
@@ -14,14 +25,22 @@ describe('generate element-plus material project correctly', () => {
     const { genResult } = res
 
     // 写入文件
-    genResult.forEach(({ fileName, path: filePath, fileContent }) => {
+    for (const { fileName, path: filePath, fileContent } of genResult) {
       fs.mkdirSync(path.resolve(__dirname, `./result/appdemo01/${filePath}`), { recursive: true })
-      fs.writeFileSync(
-        path.resolve(__dirname, `./result/appdemo01/${filePath}/${fileName}`),
-        // 这里需要将换行符替换成 CRLF 格式的
-        fileContent.replace(/\r?\n/g, '\r\n')
-      )
-    })
+
+      if (typeof fileContent === 'string') {
+        fs.writeFileSync(
+          path.resolve(__dirname, `./result/appdemo01/${filePath}/${fileName}`),
+          // 这里需要将换行符替换成 CRLF 格式的
+          fileContent.replace(/\r?\n/g, '\r\n')
+        )
+      } else if (fileContent instanceof Blob) {
+        const arrayBuffer = await fileContent.arrayBuffer()
+        const buffer = Buffer.from(arrayBuffer)
+
+        fs.writeFileSync(path.resolve(__dirname, `./result/appdemo01/${filePath}/${fileName}`), buffer)
+      }
+    }
 
     const compareOptions = {
       compareContent: true,
