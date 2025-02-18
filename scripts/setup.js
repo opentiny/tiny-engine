9fca959169460e928d5bacdad3a4db4879d49fb8
gene9831 <pacify.98@gmail.com>
2025-02-18 11:50:54 +0800

    format: add prettier version 2 to workspace and format all codes

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,3 +1,5 @@
 const { exec } = require('node:child_process')
 
-exec('pnpm -F @opentiny/tiny-engine-vite-plugin-meta-comments -F @opentiny/tiny-engine-common -F @opentiny/tiny-engine-dsl-vue build')
+exec(
+  'pnpm -F @opentiny/tiny-engine-vite-plugin-meta-comments -F @opentiny/tiny-engine-common -F @opentiny/tiny-engine-dsl-vue build'
+)
