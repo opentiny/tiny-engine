51da74cda3dcbd05b9198ffdd00c41ea769f099f
lichunn <46984087+lichunn@users.noreply.github.com>
2025-03-06 00:40:57 -0800

    fix: add test cases for routing code output (#1189)

---
+++ packages/vue-generator/test/testcases/generator/expected/appdemo01/src/views/createVm/untitledFA/UntitledA.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="page-base-style"></div>
+</template>
+
+<script setup>
+import * as vue from 'vue'
+import { defineProps, defineEmits } from 'vue'
+import { I18nInjectionKey } from 'vue-i18n'
+
+const props = defineProps({})
+
+const emit = defineEmits([])
+const { t, lowcodeWrap, stores } = vue.inject(I18nInjectionKey).lowcode()
+const wrap = lowcodeWrap(props, { emit })
+wrap({ stores })
+
+const state = vue.reactive({})
+wrap({ state })
+</script>
+<style scoped>
+.page-base-style {
+  padding: 24px;
+  background: #ffffff;
+}
+
+.block-base-style {
+  margin: 16px;
+}
+
+.component-base-style {
+  margin: 8px;
+}
+</style>
