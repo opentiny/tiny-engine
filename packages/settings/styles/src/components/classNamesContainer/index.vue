86ea4963c4143184b19abbefb0d9e2beba63d3e4
SonyLeo <746591437@qq.com>
2025-03-25 00:19:19 -0700

    fix: remove redundant listening

--- packages/settings/styles/src/components/classNamesContainer/index.vue
+++ packages/settings/styles/src/components/classNamesContainer/index.vue
@@ -167,8 +167,7 @@ watch(
     classNameState.curSelector = className
     // 多类名的选择器的暂时不支持编辑，比如 .test1.test2
     classNameState.curSelectorEditable = getCurSelectorEditable(className)
-  },
-  { immediate: true }
+  }
 )
 
 const setSelectorProps = (type, value) => {
