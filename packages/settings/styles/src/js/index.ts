ba9146088f99642614c550cfaed974307c4d4981
SonyLeo <746591437@qq.com>
2025-03-26 02:34:11 -0700

    fix: update writing

--- packages/settings/styles/src/js/index.ts
+++ packages/settings/styles/src/js/index.ts
@@ -1,16 +1,17 @@
 import { HOOK_NAME, META_SERVICE, defineService } from '@opentiny/tiny-engine-meta-register'
 import useStyle from './useStyle'
 
-const { state, ...apis } = useStyle()
-
 export default defineService({
   id: META_SERVICE.Style,
   type: 'MetaService',
-  apis: () => apis,
+  apis: () => {
+    const { state, ...apis } = useStyle()
+    return apis
+  },
   composable: {
     name: HOOK_NAME.useStyle
   },
   init: () => {
-    apis.initStylePanelWatch()
+    useStyle().initStylePanelWatch()
   }
 })
