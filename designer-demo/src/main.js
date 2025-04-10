264114c9e65a6773a62ca5d0ac3e1e35b8ca5da2
chilingling <26962197+chilingling@users.noreply.github.com>
2025-04-10 11:17:55 +0800

    feat: async import registry and mock import hotfix registry (#1298)
    * feat: async import registry and mock import hotfix registry
    
    * fix: del seperate logic

--- designer-demo/src/main.js
+++ designer-demo/src/main.js
@@ -11,16 +11,20 @@
  */
 import { defineEntry } from '@opentiny/tiny-engine-meta-register'
 import { configurators } from './configurators/'
-import registry  from '../registry'
+
 import 'virtual:svg-icons-register'
 
 async function startApp() {
-  const { init } = await import('@opentiny/tiny-engine')
+  // 这里模拟临时的 hotfix 注册表
+  const hotfixRegistry = await import('http://localhost:8090/hotfixRegistry.js')
   // 导入@opentiny/tiny-engine时，内部的依赖包也会逐个导入，可能会执行useComplie，此时需要templateHashMap。所以需要先执行一次defineEntry
-  defineEntry(registry)
+  defineEntry(hotfixRegistry.default)
+  const registry = await import('../registry')
+  const { init } = await import('@opentiny/tiny-engine')
 
   init({
-    registry,
+    // TODO: 这里支持数组，传入多个注册表
+    registry: registry.default,
     configurators,
     createAppSignal: ['global_service_init_finish']
   })
