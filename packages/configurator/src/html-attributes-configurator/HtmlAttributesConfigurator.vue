e9b6ddbff3c38e13ff5bb819b9c284ffda604519
wangwenbing <ibwrong@foxmail.com>
2025-04-11 11:18:32 +0800

    fix:修复HtmlAttributesConfigurator样式

--- packages/configurator/src/html-attributes-configurator/HtmlAttributesConfigurator.vue
+++ packages/configurator/src/html-attributes-configurator/HtmlAttributesConfigurator.vue
@@ -226,14 +226,21 @@ export default {
 }
 
 .attr-form {
-  padding: 0 10px 10px;
+  // padding: 0 10px 10px;
+  padding-top: 10px;
   .icon-close {
-    float: right;
-    margin: 10px 0;
+    position: absolute;
+    right: 10px;
+    top: 10px;
   }
   .footer {
     width: 100%;
     text-align: center;
+    // display: flex;
+    // justify-content: space-between;
+  }
+  :deep(.tiny-form-item) {
+    // display: flex;
   }
 }
 
