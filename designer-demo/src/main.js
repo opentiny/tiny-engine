678204cce11946f55146b8d60efccc7f1ee01da9
ajaxzheng <894103554@qq.com>
2025-04-18 14:51:52 +0800

    feat: 优化 main.js 中的 hotfix 注册逻辑，更新 transform.js 以增强错误提示信息

--- designer-demo/src/main.js
+++ designer-demo/src/main.js
@@ -9,7 +9,7 @@
  * A PARTICULAR PURPOSE. SEE THE APPLICABLE LICENSES FOR MORE DETAILS.
  *
  */
-import { defineEntry, tryGetAndDefineHotfixRegistry } from '@opentiny/tiny-engine-meta-register'
+import { tryGetAndDefineHotfixRegistry } from '@opentiny/tiny-engine-meta-register'
 import { configurators } from './configurators/'
 
 import 'virtual:svg-icons-register'
@@ -21,15 +21,17 @@ const fetchHotfixRegistry = async (url) => {
 
 async function startApp() {
   // 这里模拟临时的 hotfix 注册表，会根据配置的接口读取 registry并 执行 defineEntry，因为 overWrite 的逻辑需要提前读取
-  const hotfixRegistry = (await tryGetAndDefineHotfixRegistry({ url: 'http://localhost:8090/hotfixRegistry.js', request: fetchHotfixRegistry })) || {}
+  const hotfixRegistry =
+    (await tryGetAndDefineHotfixRegistry({
+      url: 'http://localhost:8090/hotfixRegistry.js',
+      request: fetchHotfixRegistry
+    })) || {}
 
-  // 导入@opentiny/tiny-engine时，内部的依赖包也会逐个导入，可能会执行useComplie，此时需要templateHashMap。所以需要先执行一次defineEntry
   const registry = await import('../registry')
-  defineEntry(registry.default)
   const { init } = await import('@opentiny/tiny-engine')
 
   init({
-    // TODO: 这里支持数组，传入多个注册表
+    // 合并多个注册表
     registry: [registry.default, hotfixRegistry],
     configurators,
     createAppSignal: ['global_service_init_finish']
