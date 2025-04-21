0245eccf722ae062b80fd0b1b804cabf919c1847
chilingling <michaelchiling1024@gmail.com>
2025-04-21 16:52:49 +0800

    feat: remove code to base64 for performance issue

--- packages/canvas/init-canvas/init-canvas.ts
+++ packages/canvas/init-canvas/init-canvas.ts
@@ -2,13 +2,21 @@ import canvasScript from './canvas?url&build=canvas'
 import canvasHtml from './canvas.html?raw'
 
 export function initCanvas(importMap = {}, importStyleUrls = []) {
-  return {
+  const res = {
     html: canvasHtml
       .replace('<!--%IMPORT_MAP%-->', `<script type="importmap">${JSON.stringify(importMap, null, 2)}</script>`)
       .replace(
         '<!--%IMPORT_STYLE%-->',
         importStyleUrls.map((styleUrl) => `<link rel="stylesheet" crossorigin="" href="${styleUrl}">`).join('\n')
       )
-      .replace('<!--%MAIN_SCRIPT%-->', `<script type="module" src="${canvasScript}"></script>`)
+      .replace(
+        '<!--%MAIN_SCRIPT%-->',
+        import.meta.env.MODE === 'development'
+          ? `<script type="module" src="${canvasScript}"></script>`
+          : // 将 $ 替换为 $$，然后在上一层的 $$ 再转义回来 $，避免被转义
+            `<script type="module">${canvasScript.replace(/\$/g, '$$$$')}</script>`
+      )
   }
+
+  return res
 }
