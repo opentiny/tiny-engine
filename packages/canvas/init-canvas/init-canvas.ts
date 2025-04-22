ff05a896958ba1595d705b5c9a6c34c76cb1b80e
chilingling <michaelchiling1024@gmail.com>
2025-04-22 10:11:48 +0800

    fix: use function replacer to avoid special replace pattern

--- packages/canvas/init-canvas/init-canvas.ts
+++ packages/canvas/init-canvas/init-canvas.ts
@@ -2,21 +2,19 @@ import canvasScript from './canvas?url&build=canvas'
 import canvasHtml from './canvas.html?raw'
 
 export function initCanvas(importMap = {}, importStyleUrls = []) {
-  const res = {
+  return {
     html: canvasHtml
       .replace('<!--%IMPORT_MAP%-->', `<script type="importmap">${JSON.stringify(importMap, null, 2)}</script>`)
       .replace(
         '<!--%IMPORT_STYLE%-->',
         importStyleUrls.map((styleUrl) => `<link rel="stylesheet" crossorigin="" href="${styleUrl}">`).join('\n')
       )
-      .replace(
-        '<!--%MAIN_SCRIPT%-->',
-        import.meta.env.MODE === 'development'
-          ? `<script type="module" src="${canvasScript}"></script>`
-          : // 将 $ 替换为 $$，然后在上一层的 $$ 再转义回来 $，避免被转义
-            `<script type="module">${canvasScript.replace(/\$/g, '$$$$')}</script>`
-      )
-  }
+      .replace('<!--%MAIN_SCRIPT%-->', () => {
+        if (import.meta.env.MODE === 'development') {
+          return `<script type="module" src="${canvasScript}"></script>`
+        }
 
-  return res
+        return `<script type="module">${canvasScript}</script>`
+      })
+  }
 }
