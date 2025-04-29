9e68773bc58d35349c87aabc00a5fce2e4e98096
chilingling <michaelchiling1024@gmail.com>
2025-04-29 14:38:08 +0800

    fix: change tryGetAndDefineHotfixRegistry to initializeHotfixRegistry

--- packages/register/src/index.ts
+++ packages/register/src/index.ts
@@ -12,7 +12,7 @@
 
 export { getMergeMeta, getMetaApi, getOptions, getMergeMetaByType, getAllMergeMeta, mergeRegistry } from './common'
 export { useCompile } from './templateHash'
-export { defineEntry, callEntry, beforeCallEntry, afterCallEntry, tryGetAndDefineHotfixRegistry } from './entryHash'
+export { defineEntry, callEntry, beforeCallEntry, afterCallEntry, initializeHotfixRegistry } from './entryHash'
 export { default as useMessage } from './useMessage'
 export { getConfigurator, addConfigurator } from './configurators'
 export * from './hooks'
