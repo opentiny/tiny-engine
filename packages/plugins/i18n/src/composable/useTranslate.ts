d03eba9c3c8267f30c282ed5d402b5aec1d3d4ac
SonyLeo <746591437@qq.com>
2025-05-06 03:33:07 -0700

    fix: remove the throw error message statement

--- packages/plugins/i18n/src/composable/useTranslate.ts
+++ packages/plugins/i18n/src/composable/useTranslate.ts
@@ -108,8 +108,8 @@ const ensureI18n = (obj: { [x: string]: any; key: string }, send?: boolean) => {
     })
 
     useCanvas().canvasApi.value?.setLocales?.(messages, true)
+    // eslint-disable-next-line
   } catch (e) {
-    throw new Error(String(e))
     // 不需要处理，有报错的词条会在画布初始化的时候统一调setLocales这个方法
   }
 
