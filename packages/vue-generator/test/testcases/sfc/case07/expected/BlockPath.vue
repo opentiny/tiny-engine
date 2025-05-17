2f7538919988596ebb9054adbcca1c731de2c1aa
0x7A7A6572 <zzerx@qq.com>
2025-05-17 18:34:12 +0800

    test(vue-generator): Add test cases to verify the use of block-relative paths
    添加新的测试用例 `case07`，用于验证在 Vue SFC 生成器中区块的相对路径是否正确解析为 `@/components/`。测试用例包括组件映射、页面 schema 和预期输出文件。

---
+++ packages/vue-generator/test/testcases/sfc/case07/expected/BlockPath.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="page-base-style">
+    <span style="display: inline-block" class="component-base-style">测试二级页面使用区块</span>
+    <block-test1a class="block-base-style"></block-test1a>
+  </div>
+</template>
+
+<script setup>
+import * as vue from 'vue'
+import { defineProps, defineEmits } from 'vue'
+import { I18nInjectionKey } from 'vue-i18n'
+import BlockTest1a from '@/components/BlockTest1a.vue'
+
+const props = defineProps({})
+
+const emit = defineEmits([])
+const { t, lowcodeWrap, stores } = vue.inject(I18nInjectionKey).lowcode()
+const wrap = lowcodeWrap(props, { emit })
+wrap({ stores })
+
+const state = vue.reactive({})
+wrap({ state })
+</script>
+<style scoped>
+.page-base-style {
+  padding: 24px;
+  background: #ffffff;
+}
+
+.block-base-style {
+  margin: 16px;
+}
+
+.component-base-style {
+  margin: 8px;
+}
+</style>
