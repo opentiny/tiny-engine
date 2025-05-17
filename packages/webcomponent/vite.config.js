616f266b2ffbd1c6978c7c0c3b070885c7447e3c
chilingling <michaelchiling1024@gmail.com>
2025-05-17 11:57:44 +0800

    fix: unexternal for webcomponent deps

--- packages/webcomponent/vite.config.js
+++ packages/webcomponent/vite.config.js
@@ -33,7 +33,7 @@ export default defineConfig({
     },
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
-      external: ['vue', 'vue-i18n', '@vue/shared'],
+      external: ['vue', 'vue-i18n'],
       output: {
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
