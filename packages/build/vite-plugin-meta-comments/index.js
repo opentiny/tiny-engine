c4aa613ced732146a7149efb47edca9e6109f569
FoundDream <ziwensong.cs@gmail.com>
2025-05-20 13:07:14 +0800

    feat: 完善vite插件api

--- packages/build/vite-plugin-meta-comments/index.js
+++ packages/build/vite-plugin-meta-comments/index.js
@@ -15,8 +15,8 @@ import { transformSFC } from './src/transform-sfc.js'
 export default function () {
   return {
     name: 'vite-plugin-generate-comments',
-    order: 'pre',
-    handler(code, id) {
+    enforce: 'pre',
+    transform(code, id) {
       if (id.endsWith('.vue')) {
         const result = transformSFC(code, id)
         return result
