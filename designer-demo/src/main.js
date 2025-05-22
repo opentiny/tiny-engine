e302a514d699b745ec90c51d0e67a94cb9e504d1
chilingling <26962197+chilingling@users.noreply.github.com>
2025-05-22 20:21:09 +0800

    fix: remove test demo (#1429)

--- designer-demo/src/main.js
+++ designer-demo/src/main.js
@@ -9,30 +9,16 @@
  * A PARTICULAR PURPOSE. SEE THE APPLICABLE LICENSES FOR MORE DETAILS.
  *
  */
-import { initHotfixRegistry } from '@opentiny/tiny-engine-meta-register'
 import { configurators } from './configurators/'
-
 import 'virtual:svg-icons-register'
 
-const fetchHotfixRegistry = async (url) => {
-  const response = await import(/* @vite-ignore */ url)
-  return response.default
-}
-
 async function startApp() {
-  // 这里模拟临时的 hotfix 注册表，会根据配置的接口读取 registry并 执行 defineEntry，因为 overWrite 的逻辑需要提前读取
-  const hotfixRegistry =
-    (await initHotfixRegistry({
-      url: 'http://localhost:8090/hotfixRegistry.js',
-      request: fetchHotfixRegistry
-    })) || {}
-
   const registry = await import('../registry')
   const { init } = await import('@opentiny/tiny-engine')
 
   init({
     // 合并多个注册表
-    registry: [registry.default, hotfixRegistry],
+    registry: [registry.default],
     configurators,
     createAppSignal: ['global_service_init_finish']
   })
