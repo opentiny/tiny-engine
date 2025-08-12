432a62ac8ef6dcd1df05e03df799f03ca71840e7
kedenLi <493601126@qq.com>
2025-08-12 15:17:27 +0800

    添加接口

--- packages/plugins/model-manager/src/composable/useModelManager.js
+++ packages/plugins/model-manager/src/composable/useModelManager.js
@@ -1,19 +1,20 @@
 import { getMetaApi, META_SERVICE } from '@opentiny/tiny-engine-meta-register'
-// 统一的模型后端接口前缀（相对路径，便于代理）
-const modelApiBase = '/material-center/api/model'
 
-export function _getModelLists(params = {}) {
-  return getMetaApi(META_SERVICE.Http).get(`${modelApiBase}/list`, { params })
+// 示例API路径，请根据实际后端接口调整
+const modelApi = '/api/model-manager/models'
+
+export function getModelList(params = {}) {
+  return getMetaApi(META_SERVICE.Http).get('http://10.234.151.79:9090/material-center/api/model/list', { params })
 }
 
 export function createModel(data) {
-  return getMetaApi(META_SERVICE.Http).post(`${modelApiBase}/save`, data)
+  return getMetaApi(META_SERVICE.Http).post('http://10.234.151.79:9090/material-center/api/model/list', data)
 }
 
-export function updateModel(data) {
-  return getMetaApi(META_SERVICE.Http).put(`${modelApiBase}/update`, data)
+export function updateModel(id, data) {
+  return getMetaApi(META_SERVICE.Http).put(`http://10.234.151.79:9090/material-center/api/model/update/${id}`, data)
 }
 
 export function deleteModel(id) {
-  return getMetaApi(META_SERVICE.Http).delete(`${modelApiBase}/delete/${id}`)
+  return getMetaApi(META_SERVICE.Http).delete(`${modelApi}/${id}`)
 }
