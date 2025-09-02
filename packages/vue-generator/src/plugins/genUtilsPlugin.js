f0761f016738592a59a1a84d29ce1fd1e31d9555
chilingling <michaelchiling1024@gmail.com>
2025-09-02 11:37:22 +0800

    fix: invalid alias default impport

--- packages/vue-generator/src/plugins/genUtilsPlugin.js
+++ packages/vue-generator/src/plugins/genUtilsPlugin.js
@@ -15,7 +15,13 @@ function genUtilsPlugin(options = {}) {
     const { content, name } = utilsConfig
     const { package: packageName, exportName, destructuring } = content
 
-    const statement = generateImportStatement({ moduleName: packageName, exportName, alias: name, destructuring })
+    // 如果 destructuring 为 false，则使用 name 作为导出名称
+    const statement = generateImportStatement({
+      moduleName: packageName,
+      exportName: destructuring ? exportName : name,
+      alias: name,
+      destructuring
+    })
     let realExportName = exportName
 
     if (name) {
