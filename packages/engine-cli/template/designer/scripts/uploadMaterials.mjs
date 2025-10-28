11705f6e9b111ad19156c85bb4acd3f63809ebe0
chilingling <michaelchiling1024@gmail.com>
2025-10-28 19:57:06 +0800

    feat: move upload script to template
    1. 将同步物料的脚本移动到用户工程以及模板中。（通过 cli 模板可以直接得到 script）
    2. 使用 FormData API 替代手动构造 multipart/form-data 请求体；增强错误处理和日志输出；增强功能详细说明
    3. 将 `updateTempmlate` 中的 `assert` 语法改为 `with` （Node.js v22 assert 语法已经被废弃）

---
+++ packages/engine-cli/template/designer/scripts/uploadMaterials.mjs
@@ -0,0 +1,62 @@
+import { Buffer } from 'node:buffer'
+import path from 'node:path'
+import dotenv from 'dotenv'
+import fs from 'fs-extra'
+import Logger from './logger.mjs'
+
+/**
+ * 同步物料资产包到后端数据库
+ * 1. 读取 env/.env.local 文件，获取后端地址。需要设置地址如：backend_url=http://localhost:9090
+ * 2. 读取 public/mock/bundle.json 文件，获取物料资产包数据
+ * 3. 将物料资产包数据通过 POST 请求上传到后端接口 /material-center/api/component/bundle/create
+ * 4. 检查数据库t_component表中数据是否更新成功
+ *
+ * 使用场景：
+ * 1. 本地已经将 bundle.json 文件进行修改，但是数据需要同步到后端数据库中。
+ * 2. 本地已经将 bundle.json 文件进行修改，但是出码仍然不正确。
+ * @returns
+ */
+async function main() {
+  const logger = new Logger('uploadMaterials')
+
+  // 先构造出.env*文件的绝对路径
+  const appDirectory = fs.realpathSync(process.cwd())
+  const resolveApp = (relativePath) => path.resolve(appDirectory, relativePath)
+  const pathsDotenv = resolveApp('env')
+  logger.info(`Start to load .env.local file from ${pathsDotenv}/.env.local`)
+  dotenv.config({ path: `${pathsDotenv}/.env.local` })
+  const { backend_url } = process.env
+
+  if (!backend_url) {
+    logger.error('backend_url is not set in .env.local file')
+    return
+  }
+
+  const bundlePath = path.join(process.cwd(), './public/mock/bundle.json')
+  logger.info(`Start to read bundle.json file from ${bundlePath}`)
+  const bundle = fs.readJSONSync(bundlePath)
+  const jsonBuffer = Buffer.from(JSON.stringify(bundle))
+
+  const requestUrl =
+    (backend_url.endsWith('/') ? backend_url.slice(0, -1) : backend_url) +
+    '/material-center/api/component/bundle/create'
+  logger.info(`Start to upload bundle.json file to ${requestUrl}`)
+  try {
+    const formData = new FormData()
+    formData.append('file', new Blob([jsonBuffer], { type: 'application/json' }), 'bundle.json')
+    const response = await fetch(requestUrl, {
+      method: 'POST',
+      body: formData
+    })
+    const data = await response.json()
+    logger.success('File uploaded successfully:', data)
+  } catch (error) {
+    logger.error('Error uploading file:', error instanceof Error ? error.message : String(error))
+  }
+}
+
+main().catch((e) => {
+  const logger = new Logger('uploadMaterials')
+  logger.error('Error uploading file:', e instanceof Error ? e.message : String(e))
+  process.exit(1)
+})
