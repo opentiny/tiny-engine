0a9adee738d0469f3b15716fa647abc12a4ef5b6
betterdancing <liulinclear@163.com>
2025-10-29 09:19:51 +0800

    Add files via upload

---
+++ packages/design-core/src/template/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <Index />
+</template>
+
+<script>
+import Index from './template/Index.vue'
+
+export default {
+  components: {
+    Index
+  }
+}
+</script>
+
+<style lang="less" scoped></style>
