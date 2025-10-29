0bca76ab82a13c3b1316ab4733f199f091641d07
littla2liyou <1446638814@qq.com>
2025-10-29 16:41:42 +0800

    fix: fix error handling in HTTP response interceptor

--- packages/runtime-renderer/src/app-function/http/index.js
+++ packages/runtime-renderer/src/app-function/http/index.js
@@ -18,9 +18,10 @@ export default (dataHandler) => {
 
   http.interceptors.response.use(dataHandler, (error) => {
     const response = error.response
-    if (response.status === 403 && response.headers && response.headers['x-login-url']) {
+    if (response && response.status === 403 && response.headers && response.headers['x-login-url']) {
       // TODO 处理无权限时，重新登录再发送请求
     }
+    return Promise.reject(error)
   })
 
   return http
