51c2127f2adebaa77730776a647a7321a44a5158
lichunn <269031597@qq.com>
2025-12-30 17:18:44 +0800

    fix:fix review

--- designer-demo/engine.config.js
+++ designer-demo/engine.config.js
@@ -1,5 +1,3 @@
-const isDevelopEnv = import.meta.env.MODE?.includes('dev')
-
 export default {
   id: 'engine.config',
   theme: 'light',
@@ -7,7 +5,5 @@ export default {
   scripts: [],
   styles: [],
   // 是否开启 TailWindCSS 特性
-  enableTailwindCSS: true,
-  // 是否启用登录
-  enableLogin: isDevelopEnv ? false : true
+  enableTailwindCSS: true
 }
