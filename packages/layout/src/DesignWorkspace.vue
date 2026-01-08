e4e8f49fabd5691c24b61737c503f70b42a5f4f2
lixuan <lx7664@163.com>
2026-01-07 22:50:13 -0800

    fix: add user info to app-center

--- packages/layout/src/DesignWorkspace.vue
+++ packages/layout/src/DesignWorkspace.vue
@@ -7,8 +7,49 @@
           class="icon-arrow-left"
           @click="$emit('backToDesigner', '')"
         ></tiny-icon-arrow-left>
-        <svg-icon name="template-logo"></svg-icon>
+        <svg-icon class="logo-icon" name="template-logo"></svg-icon>
         <span>TinyEngine</span>
+      </div>
+      <div class="user-wrapper">
+        <tiny-popover :visible-arrow="false" width="150" trigger="click">
+          <template #reference>
+            <div class="tenant-wrapper">
+              <span>{{ tenantValue.label }}</span>
+              <svg-icon class="expand-icon" name="down-arrow"></svg-icon>
+            </div>
+          </template>
+          <div class="dropdown-menu__items">
+            <div class="tenant-title">选择组织</div>
+            <div v-if="tenantList.length">
+              <div
+                class="dropdown-menu__item"
+                :class="{ 'active-menu': item.id === tenantValue?.id }"
+                v-for="item in tenantList"
+                :key="item.id"
+                @click="changeTenant(item.id)"
+              >
+                <svg-icon class="tenant-icon" name="organize"></svg-icon>
+                <span>{{ item.label }}</span>
+              </div>
+            </div>
+            <div class="empty" v-else>暂无相关数据</div>
+          </div>
+        </tiny-popover>
+        <tiny-popover :visible-arrow="false" width="150" trigger="click">
+          <template #reference>
+            <div>
+              <svg-icon class="user-icon" name="default-user"></svg-icon>
+            </div>
+          </template>
+          <div class="user-name">
+            <svg-icon class="user-icon" name="default-user"></svg-icon>{{ userInfo.username }}
+          </div>
+          <div class="divider"></div>
+          <div class="user-out" @click="logOut">
+            <svg-icon class="out-icon" name="log-out"></svg-icon>
+            退出登录
+          </div>
+        </tiny-popover>
       </div>
     </div>
     <div class="manager-index">
@@ -28,11 +69,14 @@
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { iconArrowLeft } from '@opentiny/vue-icon'
+import { getMetaApi, META_SERVICE, getMergeMeta } from '@opentiny/tiny-engine-meta-register'
+import { Popover } from '@opentiny/vue'
 
 export default {
   components: {
+    TinyPopover: Popover,
     TinyIconArrowLeft: iconArrowLeft()
   },
   props: {
@@ -51,13 +95,55 @@ export default {
       props.workspaceRegistry.find((item) => item.id === props.currentPageId) || props.workspaceRegistry[0]
     )
 
+    const enableLogin = getMergeMeta('engine.config')?.enableLogin
+    const { getUserInfo, setUserInfo, setTenantInfo, setNeedToLogin, getBaseInfo } = getMetaApi(
+      META_SERVICE.GlobalService
+    )
+
+    const userInfo = computed(() => getUserInfo())
+    const tenantList = computed(() => {
+      const info = getUserInfo()
+
+      return Array.isArray(info?.tenant)
+        ? info.tenant?.map((item) => {
+            return {
+              ...item,
+              label: item.nameEn
+            }
+          })
+        : []
+    })
+    const tenantValue = computed(() =>
+      enableLogin
+        ? tenantList.value.find((item) => item.id === getBaseInfo().tenantId) || tenantList.value[0]
+        : { ...getBaseInfo(), label: 'Public' }
+    )
+
+    const changeTenant = (id) => {
+      setTenantInfo(id).then((tenantData) => {
+        setUserInfo(tenantData)
+        localStorage.setItem('engineToken', tenantData.token)
+        const baseUrl = `${window.location.origin}${window.location.pathname}?type=app&`
+        window.location.href = `${baseUrl}tenant=${id}`
+      })
+    }
+
+    const logOut = () => {
+      localStorage.removeItem('engineToken')
+      setNeedToLogin(true)
+    }
     const handleMenuClick = (menu) => {
       activeNode.value = menu
     }
 
     return {
       activeNode,
-      handleMenuClick
+      tenantValue,
+      userInfo,
+      tenantList,
+      handleMenuClick,
+      changeTenant,
+      logOut
     }
   }
 }
@@ -74,13 +160,13 @@ export default {
     justify-content: space-between;
     align-items: center;
     border-bottom: 1px solid var(--te-common-border-bg-divider);
-    padding: 0 24px;
+    padding: 0 32px;
     .icon-arrow-left {
       font-size: 28px;
       padding-right: 10px;
       cursor: pointer;
     }
-    .svg-icon {
+    .logo-icon {
       font-size: 28px;
       padding-right: 8px;
     }
@@ -88,6 +174,22 @@ export default {
     span {
       font-size: 14px;
       font-weight: 500;
+    }
+    .user-wrapper {
+      display: flex;
+      align-items: center;
+      gap: 12px;
+      .tenant-wrapper {
+        display: flex;
+        align-items: center;
+        gap: 4px;
+        .expand-icon {
+          font-size: 16px;
+        }
+      }
+      .user-icon {
+        font-size: 32px;
+      }
     }
   }
   .manager-index {
@@ -128,6 +230,61 @@ export default {
     padding: 20px;
     width: calc(100% - 220px);
     background: var(--te-common-bg-container);
+  }
+}
+.user-name {
+  color: var(--te-layout-common-text-color-weaken);
+  .user-icon {
+    margin-right: 6px;
+    font-size: 20px;
+  }
+}
+.divider {
+  width: 100%;
+  height: 1px;
+  background: var(--te-layout-common-border-color);
+  margin: 8px 0;
+}
+.user-out {
+  display: flex;
+  align-items: center;
+  font-size: 12px;
+  padding: 4px 16px;
+  margin: 0 -16px;
+  cursor: pointer;
+  .out-icon {
+    margin-right: 6px;
+  }
+  &:hover {
+    background: var(--te-layout-workspace-dropdown-bg-color-hover);
+  }
+}
+.dropdown-menu__items {
+  .tenant-title {
+    font-size: 14px;
+    line-height: 16px;
+    margin-bottom: 14px;
+    color: var(--te-layout-common-text-color-secondary);
+  }
+  .dropdown-menu__item {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    padding: 4px 16px;
+    margin: 0 -16px;
+    line-height: 16px;
+    .tenant-icon {
+      font-size: 20px;
+    }
+    &:hover {
+      background: var(--te-layout-workspace-dropdown-bg-color-hover);
+    }
+  }
+  .empty {
+    color: var(--te-layout-common-text-color-weaken);
+  }
+  .active-menu {
+    background: var(--te-layout-workspace-dropdown-bg-color-active);
   }
 }
 </style>
