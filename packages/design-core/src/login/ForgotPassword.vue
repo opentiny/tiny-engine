5364fc131e744521e8a2dfeb061de94adb115fcd
lichunn <269031597@qq.com>
2026-05-29 16:59:42 +0800

    fix：消除不必要修改

--- packages/design-core/src/login/ForgotPassword.vue
+++ packages/design-core/src/login/ForgotPassword.vue
@@ -157,6 +157,7 @@ export default {
     )
     return {
       state,
+      isReady,
       handleForgot,
       toLogin
     }
--- packages/design-core/src/login/Index.vue
+++ packages/design-core/src/login/Index.vue
@@ -98,10 +98,13 @@ export default {
   }
   :deep(.tiny-button.tiny-button.tiny-button.tiny-button) {
     width: 100%;
-    background: #595959;
+    background: #191919;
     height: 32px;
     margin-top: 20px;
     font-size: 14px;
+  }
+  :deep(.tiny-button.tiny-button.tiny-button.tiny-button.tiny-button--primary) {
+    border: none;
   }
   :deep(.tiny-input.tiny-input .tiny-input__inner.tiny-input__inner) {
     height: 32px;
