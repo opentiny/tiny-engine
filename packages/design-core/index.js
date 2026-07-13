4be4445385b441a8a2295f07f57b96e307db6e5f
lichunn <269031597@qq.com>
2026-07-13 14:15:48 +0800

    fix:流水线报错

--- packages/design-core/index.js
+++ packages/design-core/index.js
@@ -1,6 +1,6 @@
-import '@opentiny/tiny-engine-common/dist/style.css'
-import '@opentiny/tiny-engine-plugin-robot/dist/style.css'
-import '@opentiny/tiny-engine-canvas/dist/index.css'
+import '@opentiny/tiny-engine-common/style.css'
+import '@opentiny/tiny-engine-plugin-robot/style.css'
+import '@opentiny/tiny-engine-canvas/style.css'
 
 export { init } from './src/init'
 
