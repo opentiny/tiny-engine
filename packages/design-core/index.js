d493b7ff552e15bf4cf4f9f73cf64d15ebbf51bd
lichunn <269031597@qq.com>
2026-07-13 11:29:42 +0800

    fix:主包没有稳定聚合子包样式

--- packages/design-core/index.js
+++ packages/design-core/index.js
@@ -1,3 +1,7 @@
+import '@opentiny/tiny-engine-common/dist/style.css'
+import '@opentiny/tiny-engine-plugin-robot/dist/style.css'
+import '@opentiny/tiny-engine-canvas/dist/index.css'
+
 export { init } from './src/init'
 
 export * from './re-export'
